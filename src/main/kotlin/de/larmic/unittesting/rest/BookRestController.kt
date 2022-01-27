32319f19228dafa0b7878ab4aafc44a0e5e76208
Lars Michaelis <l.michaelis.ext@ewe.de>
2022-01-27 17:41:13 +0100

    feat: clean up code

--- src/main/kotlin/de/larmic/unittesting/rest/BookRestController.kt
+++ src/main/kotlin/de/larmic/unittesting/rest/BookRestController.kt
@@ -1,8 +1,6 @@
 package de.larmic.unittesting.rest
 
 import com.fasterxml.jackson.annotation.JsonFormat
-import de.larmic.unittesting.database.Author
-import de.larmic.unittesting.database.Book
 import de.larmic.unittesting.database.BookRepository
 import de.larmic.unittesting.rest.mapper.DtoToDomainMapper
 import org.springframework.web.bind.annotation.PostMapping
@@ -11,9 +9,6 @@ import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
 
 // Beispiel für "Tested eure Frameworks"
-
-// wie das mapping geschieht ist whitebox?!
-// beispiel mit mocking framework injecten (z.B. mapstruct oder selbst schreiben) -> dann ein Integrationstests?
 @RestController
 class BookRestController(private val dtoToDomainMapper: DtoToDomainMapper, private val bookRepository: BookRepository) {
 
@@ -25,5 +20,5 @@ class BookRestController(private val dtoToDomainMapper: DtoToDomainMapper, priva
 
 }
 
-data class BookDto(val title: String, val author: AuthorDto, @JsonFormat(pattern="dd.MM.yyyy") val createDate: LocalDate)
+data class BookDto(val title: String, val author: AuthorDto, @JsonFormat(pattern = "dd.MM.yyyy") val createDate: LocalDate)
 data class AuthorDto(val firstName: String, val lastName: String)
